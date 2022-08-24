138dd59aca871de2cbcd8e7a8f4d636614cf527e
himanshudestiny <himanshudestiny1997@gmail.com>
2022-08-25 00:10:43 +0530

    added day3

--- script/findparking.js
+++ script/findparking.js
@@ -7,3 +7,70 @@ dropdown.onclick= function () {
 function show(anything) {
     document.querySelector(".textBox").value=anything;
 }
+
+
+
+
+let LosAngeles = async () => {
+    try{
+     
+        let res=await fetch(`http://localhost:3000/LosAngeles`);
+        let data=await res.json();
+        console.log(data);
+        localStorage.setItem("data",JSON.stringify(data));
+        let place="Los Angeles";
+        localStorage.setItem("place",JSON.stringify(place));
+        window.location.href="slots.html";
+    }
+    catch (err) {
+        console.log("err",err);
+    }
+}
+
+let Omaha = async () => {
+    try{
+     
+        let res=await fetch(`http://localhost:3000/Omaha`);
+        let data=await res.json();
+        console.log(data);
+        localStorage.setItem("data",JSON.stringify(data));
+        let place="Omaha";
+        localStorage.setItem("place",JSON.stringify(place));
+        window.location.href="slots.html";
+    }
+    catch (err) {
+        console.log("err",err);
+    }
+}
+
+let AnnArbor = async () => {
+    try{
+     
+        let res=await fetch(`http://localhost:3000/AnnArbor`);
+        let data=await res.json();
+        console.log(data);
+        localStorage.setItem("data",JSON.stringify(data));
+        let place="Ann Arbor";
+        localStorage.setItem("place",JSON.stringify(place));
+        window.location.href="slots.html";
+    }
+    catch (err) {
+        console.log("err",err);
+    }
+}
+
+let Tempe = async () => {
+    try{
+     
+        let res=await fetch(`http://localhost:3000/Tempe`);
+        let data=await res.json();
+        console.log(data);
+        localStorage.setItem("data",JSON.stringify(data));
+        let place="Tempe";
+        localStorage.setItem("place",JSON.stringify(place));
+        window.location.href="slots.html";
+    }
+    catch (err) {
+        console.log("err",err);
+    }
+}
